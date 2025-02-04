7e18effb001d4c62b482ca364b0cdf650a44f431
Vixtor120 <victorhidsan@campus.monlau.com>
2025-02-04 20:14:08 +0100

    primer commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    document.querySelector('h1').style.display = 'none';
+    document.getElementById('playButton').style.display = 'none';
+});

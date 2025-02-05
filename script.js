c8cd0adc7b3b249ea5fccae3569469c9e0aba6e8
Vixtor120 <victorhidsan@campus.monlau.com>
2025-02-05 20:13:38 +0100

    segundo commit

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 document.getElementById('playButton').addEventListener('click', function() {
-    document.querySelector('h1').style.display = 'none';
-    document.getElementById('playButton').style.display = 'none';
+    document.querySelector('h1').style.opacity = '0';
+    document.querySelector('h1').style.transform = 'scale(0.9)';
+    document.getElementById('playButton').style.opacity = '0';
+    document.getElementById('playButton').style.transform = 'scale(0.9)';
+    
+    setTimeout(function() {
+        document.querySelector('h1').style.display = 'none';
+        document.getElementById('playButton').style.display = 'none';
+        
+        const canvas = document.querySelector('canvas');
+        const ctx = canvas.getContext('2d');
+        const img = new Image();
+        img.src = 'images/Nivel1/Oceano/fondo_mar.png';
+        img.onload = function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        };
+        
+        canvas.style.display = 'block';
+        setTimeout(function() {
+            canvas.style.opacity = '1';
+        }, 10);
+    }, 1000);
 });
